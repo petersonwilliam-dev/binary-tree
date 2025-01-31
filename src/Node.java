1c525dc22b08a2f982a2ec1b22bd9b8dd8a8099c
Peterson William <petersondev.profissional@gmail.com>
2025-01-30 22:26:12 -0300

    Initial commit

---
+++ src/Node.java
@@ -0,0 +1,33 @@
+public class Node {
+
+    private Node left, right, parent;
+    private int data;
+
+    public Node(int data) {
+        this.data = data;
+    }
+
+    public Node getLeft() {
+        return left;
+    }
+
+    public void setLeft(Node left) {
+        this.left = left;
+    }
+
+    public Node getRight() {
+        return right;
+    }
+
+    public void setRight(Node right) {
+        this.right = right;
+    }
+
+    public int getData() {
+        return data;
+    }
+
+    public void setData(int data) {
+        this.data = data;
+    }
+}
---
+++ src/Tree.java
@@ -0,0 +1,10 @@
+public class Tree {
+
+    private Node root;
+
+    public Tree() {}
+
+    public void insert(int data) {
+
+    }
+}
